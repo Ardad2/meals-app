066ebf54e16cf19f4c1d49ad10940f7b2059046c
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-06-07 21:07:21 -0700

    grid is now colored by the grid item's property

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,13 @@
-import { Pressable, View, Text, StyleSheet } from 'react-native';
+import { Pressable, View, Text, StyleSheet, Platform } from 'react-native';
 
 function CategoryGridTile({title, color}) {
     return ( 
-    <View style={styles.gridItem}>
-        <Pressable style={styles.button}> 
+    <View style={[styles.gridItem, { backgroundColor: color}]}>
+        <Pressable 
+        android_ripple={{color:'#ccc'}} 
+        style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed :  null]}> 
             <View style={styles.innerContainer}>
-                <Text>{title}</Text>
+                <Text style={styles.title}>{title}</Text>
             </View>
         </Pressable> 
     </View> 
@@ -26,9 +28,14 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowOffset: { width: 0, height: 2},
         shadowRadius: 8,
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible'
     },
     button: {
         flex: 1
+    },
+
+    buttonPressed: {
+        opacity: 0.5
     },
 
     innerContainer: {
@@ -36,6 +43,11 @@ const styles = StyleSheet.create({
         padding: 16,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
     }
 }
 );
