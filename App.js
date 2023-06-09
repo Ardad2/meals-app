3511bfe6adf2acc40f21174d93e841ec2c94626b
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-06-08 21:48:51 -0700

    added stack navigation

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 
 export default function App() {
@@ -9,6 +12,9 @@ export default function App() {
     <>
     <StatusBar style='light'/>
     <NavigationContainer>
+      <Stack.Navigator> 
+        <Stack.Screen name="MealCategories" component={CategoriesScreen}/>
+      </Stack.Navigator>
     <CategoriesScreen />
     </NavigationContainer>
     </>
