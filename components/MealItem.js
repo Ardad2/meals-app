f73f28367bf905a18759e2caf26b0aaeccede47a
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-06-09 22:13:20 -0700

    finished off meal details

--- components/MealItem.js
+++ components/MealItem.js
@@ -6,16 +6,20 @@ function MealItem({ id, title, imageUrl, duration, complexity, affordability})
 {
     const navigation = useNavigation();
 
+    function selectMealItemHandler() {
     navigation.navigate('MealDetail', {
         mealId: id
     });
+    }
 
     return (
         <View style={styles.mealItem}>
 
 
             <Pressable android_ripple={{ color: '#ccc' }} 
-            style={({ pressed }) => ( pressed ? styles.buttonPressed: null)} > 
+            style={({ pressed }) => ( pressed ? styles.buttonPressed: null)}
+            onPress={selectMealItemHandler}
+            > 
 
             <View style={styles.innerContainer}> 
             <View > 
@@ -23,14 +27,11 @@ function MealItem({ id, title, imageUrl, duration, complexity, affordability})
             <Text style={styles.title}>{title}</Text>
             </View>
 
-
-            <View style={styles.details}>
-
-                <Text style={styles.detailItem}>{duration}m</Text>
-                <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-                <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-
-            </View>
+            <MealDetails 
+            duration = {duration}
+            affordability = {affordability}
+            complexity = { complexity }
+            />
 
 
             </View> 
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,8 +1,34 @@
-import { Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
-function MealDetailScreen() {
+import MealDetails from '../components/MealDetails';
+
+import { MEALS } from '../data/dummy-data';
+
+function MealDetailScreen({route}) {
+
+    const mealId = route.params.mealId;
+
+    const selectedMeal = MEALS.find( (meal) => meal.id === mealId)
+
     return (
-        <Text> </Text> 
+        <View> 
+        <Image source={ {  uri: selectedMeal.imageUrl   }  }/>
+
+        <Text>{selectedMeal.title}</Text>
+        <MealDetails duration={selectedMeal.duration} complexity={selectedMeal.complexity} affordability={selectedMeal.affordability} />
+
+        <Text>Ingredients</Text>
+        {selectedMeal.ingredients.map( (ingredient) => (
+            <Text key={ingredient}>{ingredient}</Text>
+        ) )}
+
+        <Text>Steps</Text>
+        {selectedMeal.steps.map( (step) => (
+            <Text key={step}>{step}</Text>
+        ) )}
+
+        </View>
+
         );
 
 }
